90598daeb44b0fbdce406ce1656efbbd92a4647d
Johnny Teles Gil <141787979+johnnytelesdev@users.noreply.github.com>
2025-04-21 22:57:41 -0300

    inicio de adicionar informações
    Adicionando logo

---
+++ aula01/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logoSpotify from "../assets/logo/spotify-logo.png";
+
+const Header = () => {
+  return (
+    <div>
+ 
+       <img src= {logoSpotify} alt="Spotfy" /> 
+        
+        
+    </div>
+  );
+};
+
+export default Header
